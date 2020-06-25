564cfc385a6e52a0f83223796bc6c39fc35ab6b1
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-06-25 21:08:34 +0100

    connected to mongoose

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const dbConnect = require('./models/dbConnect')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
+
+dbConnect()
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World...')
---
+++ models/dbConnect.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const connetToDB = async () => {
+  try {
+    mongoose.connect('mongodb://localhost:27017/code-chat', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+      console.log('Mongoose connected...')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = connetToDB
