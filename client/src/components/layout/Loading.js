f69c757ce0ceb27b863f930ea134a3778c4f0a66
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-06-26 12:28:52 +0100

    created loading component

---
+++ client/src/components/layout/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Loading = () => {
+  return <Loader>Loading</Loader>
+}
+
+const Loader = styled.div`
+  font-size: 25px;
+  text-align: center;
+  align-items: center;
+`
+
+export default Loading
