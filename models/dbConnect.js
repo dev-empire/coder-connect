d6480eb8ae531b95808e7cf11b2829e0f3e96c81
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-06-26 20:27:48 +0100

    updated server setup

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -5,6 +5,7 @@ const connetToDB = async () => {
     mongoose.connect('mongodb://localhost:27017/code-chat', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     }),
       console.log('Mongoose connected...')
   } catch (error) {
--- routes/users.js
+++ routes/users.js
@@ -9,18 +9,46 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    age: req.body.age,
-    password: req.body.password,
+  const { name, email, password, phone, age } = req.body
+
+  /* check if all fields are filled*/
+
+  if (!name || !email || !password || !age || !phone) {
+    res.status(400).json({ msg: 'Please Enter All Fields' })
+  }
+
+  /* VALIDATE USER'S REGISTRATION */
+
+  User.findOne({ email }).then((user) => {
+    if (user) return res.status(400).json({ msg: 'User  Already Exits' })
+
+    const newUser = new User({
+      name,
+      email,
+      phone,
+      age,
+      password,
+    })
+    /* HASH PASSWORD */
+    /* 
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err
+        newUser.password = hash
+        newUser.save()
+          .then(user => {
+            name: user.name
+            email: user.email
+            phone: user.phone
+            age: user.age
+            password: user.password
+          })
+      })
+    })
+    */
   })
   try {
-    await user.save().then((user) => res.json(user))
-    // res.setHeader('Content-Type', 'appliction/json')
-    // res.json(user)
-    // res.json(req.body).status(200)
+    await newUser.save().then((user) => res.json(user))
   } catch (error) {
     console.error(error)
   }
