29f8cdb2c7f7a198e655e4334d1741046eacc49a
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-06-19 14:23:19 +0100

    removed all console logs

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -9,13 +9,14 @@ const {
 	createRefreshToken,
 } = require('../utils/jwtFunctions');
 const auth = require('../middleware/auth');
+const { validateFields } = require('../utils/validationUtil');
 
 /**
  * Get all users endpoint
  * @returns An array of all the users
  */
 router.get('/users', async (req, res) => {
-	const result = await User.find({});
+	const result = await User.find({}).select('-password');
 	res.status(200).json(result);
 });
 
@@ -38,70 +39,61 @@ router.get('/:id', async (req, res) => {
  */
 router.post('/users', async (req, res) => {
 	const user = await User.findOne({ email: req.body.email });
-	if (user) return res.status(400).json({ msg: 'User Already Exits' });
+	if (user)
+		return res.status(400).json({
+			success: false,
+			statusCode: 400,
+			info: 'User already exists',
+		});
 
 	const { name, email, password } = req.body;
 
-	/* check if all fields are filled*/
-
-	// Name field
-	if (name.length < 3) {
-		res.status(400).json({ msg: 'Please Enter All Fields' });
-	}
-
-	// Email field
-	if (!email.includes('@') || email.length < 6) {
-		res.status(400).json({ msg: 'Provide a valid email' });
-	}
-
-	// Password field
-	if (password.length < 4) {
-		res.status(400).json({ msg: 'Password must greater than 5' });
-	}
-
+	const err = validateFields(email, name, password);
+	if (err) res.status(400).json({ err });
 	const hashedPassword = await hashPassword(password);
 
 	/* VALIDATE USER'S REGISTRATION */
 
-	const newUser = new User({
+	// const newUser = new User({
+	// 	name,
+	// 	email,
+	// 	password: hashedPassword,
+	// });
+
+	const input = {
 		name,
 		email,
 		password: hashedPassword,
-	});
+	};
 
-	await newUser.save().then((user) => {
-		jwt.sign(
-			{ id: user._id },
-			process.env.JWT_SECRET,
-			{ expiresIn: '3600' },
-			(err, token) => {
-				if (err) throw err;
-				res.json({
-					token,
-					name: user.name,
-					email: user.email,
-				});
-				req.cookies('connect', token, {
-					httpOnly: true,
-					maxAge: 1000 * 60 * 60,
-				});
-			}
-		);
-	});
+	const n = await User.create(input);
+	console.log(n);
+	// const token = createRefreshToken(newUser);
+	// console.log(token);
+	// sendRefreshToken(res, token);
+	return res.json({ n });
 });
 
-router.post('/user/login', async (req, res) => {
+router.post('/user/login', async (req, res, next) => {
 	const { email, password } = req.body;
 	const user = await User.findOne({ email });
 	try {
 		if (!user) {
-			throw new Error('could not find user');
+			res.status(400).json({
+				status: 'failed',
+				statusCode: 400,
+				info: 'Could not find user',
+			});
 		}
 
 		const valid = await compare(password, user.password);
 
 		if (!valid) {
-			throw new Error('bad password');
+			res.status(400).json({
+				status: 'failed',
+				statusCode: 400,
+				info: 'Incorrect password',
+			});
 		}
 		const token = createRefreshToken(user);
 		console.log(token);
@@ -112,8 +104,8 @@ router.post('/user/login', async (req, res) => {
 			statusCode: 200,
 			user,
 		});
-	} catch (error) {
-		throw new Error(error);
+	} catch (err) {
+		next(err);
 	}
 });
 
@@ -127,8 +119,8 @@ router.get('/user/auth', auth, async (req, res, next) => {
 				statusCode: 200,
 				user: decoded,
 			});
-		} catch (e) {
-			next(e);
+		} catch (err) {
+			next(err);
 		}
 	}
 });
---
+++ server/src/utils/validationUtil.js
@@ -0,0 +1,35 @@
+const validateFields = (email, name, password) => {
+	// Name field
+	if (name.length < 3) {
+		const err = {
+			success: false,
+			statusCode: 400,
+			info: 'name must at least be 3',
+		};
+		return err;
+	}
+
+	// Email field
+	if (!email.includes('@') || email.length < 6) {
+		const err = {
+			success: false,
+			statusCode: 400,
+			info: 'invalid email',
+		};
+		return err;
+	}
+
+	// Password field
+	if (password.length < 4) {
+		const err = {
+			success: false,
+			statusCode: 400,
+			info: 'password must be at least 4',
+		};
+		return err;
+	}
+};
+
+module.exports = {
+	validateFields,
+};
