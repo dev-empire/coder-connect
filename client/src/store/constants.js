874a8c91f620001611f49da6c56731ad5625d33b
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-06-22 10:19:52 +0100

    commit changes

---
+++ client/src/store/constants.js
@@ -0,0 +1,4 @@
+export const GET_ALL_USERS = 'chat-hub/GET_ALL_USERS'
+export const CREATE_USER = 'chat-hub/CREATE_USER'
+export const LOGIN_USER = 'chat-hub/LOGIN_USER'
+export const CREATE_ERROR = 'chat-hub/CREATE_ERROR'
