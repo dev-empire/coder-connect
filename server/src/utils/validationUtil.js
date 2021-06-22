213c34a40eb0e92a3530e9190d65101edef7dcdb
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-06-22 19:27:45 +0100

    custom field added for better error handling

--- server/src/utils/validationUtil.js
+++ server/src/utils/validationUtil.js
@@ -2,7 +2,7 @@ const validateFields = (email, name, password) => {
 	// Name field
 	if (name.length < 3) {
 		const err = {
-			success: false,
+			field: 'name',
 			statusCode: 400,
 			info: 'name must at least be 3',
 		};
@@ -12,7 +12,7 @@ const validateFields = (email, name, password) => {
 	// Email field
 	if (!email.includes('@') || email.length < 6) {
 		const err = {
-			success: false,
+			field: 'email',
 			statusCode: 400,
 			info: 'invalid email',
 		};
@@ -22,7 +22,7 @@ const validateFields = (email, name, password) => {
 	// Password field
 	if (password.length < 4) {
 		const err = {
-			success: false,
+			field: 'password',
 			statusCode: 400,
 			info: 'password must be at least 4',
 		};
