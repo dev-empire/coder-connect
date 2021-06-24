3ed31a0fd515ac4f57cf6031d5ca8438a57301a3
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-06-24 10:27:24 +0100

    handles auth

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginUser } from '../../store/ducks/user'
 import { useFormik } from 'formik'
+import { Redirect, useHistory } from 'react-router'
 
 const Login = () => {
+  const history = useHistory()
   const dipatch = useDispatch()
-  const { error } = useSelector(state => state.user)
+  const { error, isAuthenticated } = useSelector(state => state.user)
+  console.log(isAuthenticated)
   const emailFieldError = error && error.field === 'email'
   const passwordFieldError = error && error.field === 'password'
   const formik = useFormik({
@@ -15,35 +18,41 @@ const Login = () => {
     },
     onSubmit: values => {
       dipatch(loginUser(values))
+      formik.resetForm()
     },
   })
+
   return (
     <div style={{ margin: '2em 0' }}>
-      <form className="ui form" onSubmit={formik.handleSubmit}>
-        <div className="field">
-          <label htmlFor="email">Email</label>
-          {emailFieldError && <div>{error.info} </div>}
-          <input
-            type="email"
-            name="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            placeholder="Email"
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="password">Password</label>
-          {passwordFieldError && <div>{error.info} </div>}
-          <input
-            type="password"
-            name="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-            placeholder="Password"
-          />
-        </div>
-        <input className="ui secondary button" type="submit" value="Log In" />
-      </form>
+      {isAuthenticated ? (
+        <Redirect to="/" />
+      ) : (
+        <form className="ui form" onSubmit={formik.handleSubmit}>
+          <div className="field">
+            <label htmlFor="email">Email</label>
+            {emailFieldError && <div>{error.info} </div>}
+            <input
+              type="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              placeholder="Email"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password</label>
+            {passwordFieldError && <div>{error.info} </div>}
+            <input
+              type="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              placeholder="Password"
+            />
+          </div>
+          <input className="ui secondary button" type="submit" value="Log In" />
+        </form>
+      )}
     </div>
   )
 }
